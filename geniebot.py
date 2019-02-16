c4da934a68d120ff8ba2d71221fb3a036124dcb6
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-02-17 04:38:04 +0800

    Create geniebot.py

---
+++ geniebot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+    
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='Spotify with Tatsumaki', type = 2))
+    print('Codes are working perfectly fine! you may use it now!') 
+
+@client.event
+async def on_message(message):
+    if message.content == '@everyone':
+        await client.send_message(message.channel,'@here Orayt rock and roll boom boom!')
+    if ('@everyone') in message.content:
+       await client.delete_message(message)
+       
+
+        
+client.run(str(os.environ.get('TOKEN')))
