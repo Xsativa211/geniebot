2b8d29c493ee70c0ec6d543cbc565cec6cb67070
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-20 19:21:45 +0800

    Update geniebot.py

--- geniebot.py
+++ geniebot.py
@@ -22,7 +22,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == '!zeta':
-        await client.send_message.user('Hello there!')
+       await client.send_message(ctx.message.author, 'Hello!')
     if message.content == '!test':
         await client.send_message(message.channel,'Hello this is just a test from zetaRO')
     if ('!test') in message.content:
