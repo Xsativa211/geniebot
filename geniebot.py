9e27e6896c412e1bda7b91f5b5b40849c647bd57
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-21 05:19:58 +0800

    Update geniebot.py

--- geniebot.py
+++ geniebot.py
@@ -36,6 +36,14 @@ async def on_message(message):
         output = message.content.replace('!update ', '')
         await client.send_message(message.channel, 'Hello Zeta Players we have a new update! \n ' + output)
     if ('!br') in message.content:
-       await client.delete_message(message)       
+       await client.delete_message(message)
+    
+    if message.content.startswith("!zhelp"):
+        em = discord.Embed(title="Zeta Bot Commands", description="Shows you the bot commands available for you!", colour=0x551d6d)
+        em.set_thumbnail(url=message.server.icon_url)
+        embed.add_field(name="official", value="Gives you the official Facebook Page and Community Page.", inline=False)
+        embed.add_field(name="invite", value="Gives you the Discord Server invitation.", inline=False)
+        embed.add_field(name="guide", value="To Be Announce", inline=False)
+        await client.send_message(message.channel, embed=em)    
         
 client.run(str(os.environ.get('TOKEN')))
