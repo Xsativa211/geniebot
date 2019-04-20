42fe6a40f9a4b5227173a81912017683d8f77842
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-21 00:31:55 +0800

    Update geniebot.py

--- geniebot.py
+++ geniebot.py
@@ -28,20 +28,18 @@ async def on_message(message):
     if message.content == "!commands":
        await client.send_message(message.author, "Commands will be released in the future :)")
 
-@client.event()
-async def on_message(message):
-    if message.content == "info":
-       embed = discord.Embed(title="ZetaRO Bot", description="#ZetaRO", color=0xeee657)
+@bot.command()
+async def help(ctx):
+embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
 
-        # give info about you here
-       embed.add_field(name="Author", value="ZetaRO")
+embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
+embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
+embed.add_field(name="$help", value="Gives this message", inline=False)
 
-        # Shows the number of servers the bot is member of.
-       embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
-
-        # give users a link to invite thsi bot to their server
-       embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
-       await ctx.send(embed=embed)
+await ctx.send(embed=embed)
 
     if message.content.startswith('!br'):
         output = message.content.replace('!br ', '')
