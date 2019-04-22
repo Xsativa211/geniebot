0e15925b7beb897682f48dca3f255d8e172fb0ed
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-22 16:21:36 +0800

    Update geniebot.py

--- geniebot.py
+++ geniebot.py
@@ -26,7 +26,12 @@ async def on_message(message):
     if ('!official') in message.content:
        await client.delete_message(message) 
     if message.content == '!invitation':
-        await client.send_message(message.channel,'Official Discord Server Invitation Link https://discord.gg/AVznxUU')    
+        await client.send_message(message.channel,'Official Discord Server Invitation Link https://discord.gg/AVznxUU')
+    if message.content == '@request':
+        await client.send_message(message.author,'@request is a command to send a Private Message from the staff in the game, please use the #request channel on discord if you have a concern')
+   
+    if ('@request') in message.content:
+       await client.delete_message(message)
     if ('!official') in message.content:
        await client.delete_message(message)
     if ('invitation') in message.content:
@@ -37,7 +42,21 @@ async def on_message(message):
         await client.send_message(message.channel, 'Hello Zeta Players we have a new update! \n ' + output)
     if ('!zbotupdate') in message.content:
        await client.delete_message(message)
-  
+    
+    if message.content.startswith('!hbc'):
+        for user in message.mentions:
+            msg = 'Hello Everyone! Helper {} is currently active in the game and discord!\nIf you have a questions on how to start from the server feel free to send us a message via @request.\nAll @requests from newbies will be prioritize. so please wait for me to respond on your requests\n\nThank you!'.format(user.mention)
+            await client.send_message(message.channel, msg)
+    if message.content.startswith('!helper'):
+        for user in message.mentions:
+            msg = ' Hello {} If you have a question look for people who have the role "Support Team" or user with the violet name\nThey can help you regarding on your concerns and questions! or leave us a message at #request channel '.format(user.mention)
+            await client.send_message(message.channel, msg)            
+    if ('!hbc') in message.content:
+       await client.delete_message(message)
+    if ('!helper') in message.content:
+       await client.delete_message(message)
+    
+    
     if message.content.startswith("!zhelp"):
         em = discord.Embed(title="Zeta Bot Commands", description="Show you the bot commands available for you!", colour=0xcc780a)
         em.set_thumbnail(url=message.server.icon_url)
@@ -45,6 +64,7 @@ async def on_message(message):
         em.add_field(name="!official", value="Gives you the official link Facebook Page and Community Group.", inline=False)
         em.add_field(name="!invite", value="Gives you the Discord Server invitation.", inline=False)
         em.add_field(name="!guide", value="To Be Announce!", inline=False)
+        em.add_field(name="@request", value="Send you the usage of the command", inline=False)        
         em.add_field(name="!forums", value="Link to be updated soon!", inline=False)
         em.set_footer(text="Server Discord and Bot Owner Jhake#4303")
         await client.send_message(message.channel, embed=em)    
