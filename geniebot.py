8889add3aded2bd0f71bc988cb3e84505dc0f300
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-23 00:17:43 +0800

    Update geniebot.py

--- geniebot.py
+++ geniebot.py
@@ -59,7 +59,7 @@ async def on_message(message):
         await client.delete_message(message)
     if message.content == '!hbcjoe':
         await client.send_message(message.channel,'Hello <@!324825272916639744> is currently active in game and discord!\nSend her a message if you have a questions\n```Her In Game Name Helper is <your in game name>```')    
-    if ('!hbctam') in message.content:
+    if ('!hbcjoe') in message.content:
         await client.delete_message(message)  
 
     
